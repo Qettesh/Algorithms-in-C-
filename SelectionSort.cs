225fe866506f9d22f988417819817a3589b90672
きよみず(KIyomizu)あゆみ(Ayumi) <kiyo.a@outlook.com>
2025-04-01 10:55:49 -0400

    Selection Sort
    Selection sort with unit test results.

---
+++ SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Suite_Lib
+{
+    public class SelectionSort : Sort
+    {
+        public static void Selection_Sort(int[] array)
+        {
+            for (int partIndex = array.Length - 1; partIndex > 0;  partIndex--)
+            {
+                int largestAt = 0;
+                for (int i = 1; i <= partIndex; i++)
+                {
+                    if (array[i] > array[largestAt])
+                    {
+                        largestAt = i;
+                    }
+                }
+
+                Swap(array, largestAt, partIndex);
+            }
+        }
+    }
+}
