c88d724120302077647a03d6d9a0db3f4dda77d7
きよみず(KIyomizu)あゆみ(Ayumi) <kiyo.a@outlook.com>
2025-05-19 15:04:35 -0400

    Add files via upload

---
+++ LinkedListQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Suite_Lib
+{
+    public class LinkedListQueue<T> : IEnumerable<T>
+    {
+        public int Count => _list.Count;
+        public bool IsEmpty => Count == 0;
+
+        public object? Capacity { get; set; }
+
+        private readonly SingleLinkedList<T> _list = new SingleLinkedList<T>();
+
+        public void Enqueue(T item)
+        {
+            _list.AddLast(item);
+        }
+
+        public void Dequeue()
+        {
+            _list.RemoveFirst();
+        }
+
+        public T Peek()
+        {
+            return _list.Head.Value;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LinkedListQueueTests.cs
@@ -0,0 +1,56 @@
+﻿using Algorithm_Suite_Lib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Suite_Lib.Tests
+{
+    [TestFixture]
+    public class LinkedListQueueTests
+    {
+        private LinkedListQueue<int> _queue;
+        [SetUp]
+        public void Init()
+        {
+            _queue = new LinkedListQueue<int>();
+        }
+
+        [Test]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            Assert.That(_queue.IsEmpty, Is.True);
+        }
+
+        [Test]
+        public void Count_PushOneItem_ReturnsOne()
+        {
+            _queue.Enqueue(1);
+
+            Assert.That(1, Is.EqualTo(_queue.Count));
+            Assert.That(_queue.IsEmpty, Is.False);
+        }
+
+        [Test]
+        public void Peek_PushTwoItems_ReturnsHeadItem()
+        {
+            _queue.Enqueue(1);
+            _queue.Enqueue(2);
+
+            _queue.Dequeue();
+
+            Assert.That(2, Is.EqualTo(_queue.Peek()));
+        }
+
+        [Test]
+        public void Peek_PushTwoItemsAndPop_ReturnsHeadElement()
+        {
+            _queue.Enqueue(1);
+            _queue.Enqueue(2);
+
+            Assert.That(1, Is.EqualTo(_queue.Peek()));
+        }
+    }
+}
