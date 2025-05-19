693707a4be7e8b899d425e1f7f3259d7441da285
きよみず(KIyomizu)あゆみ(Ayumi) <kiyo.a@outlook.com>
2025-05-19 12:24:55 -0400

    Add files via upload
    Implementation of a Queue Array /w unit tests

---
+++ QueueArray.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Suite_Lib
+{
+    public class QueueArray<T> : IEnumerable<T>
+    {
+        private T[] _queueArray;
+        private int _head;
+        private int _tail;
+        public int Count => _tail - _head;
+        public bool IsEmpty => Count == 0;
+
+        public int Capacity => _queueArray.Length;
+
+        public QueueArray()                       //Constructor
+        {
+            const int defaultCapacity = 8;
+            _queueArray = new T[defaultCapacity];
+        }
+
+        public QueueArray(int capacity)
+        {
+            _queueArray = new T[capacity];            //Constructor
+        }
+
+        
+
+        public void Enqueue(T item)
+        {
+            if(_queueArray.Length == _tail)
+            {
+                T[] largerArray = new T[Count * 4];
+                Array.Copy(_queueArray, largerArray, Count);
+                _queueArray = largerArray;
+            }
+            _queueArray[_tail++] = item;
+        }
+
+        public void Dequeue()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException();
+
+            _queueArray[_head++] = default(T);
+
+            if (IsEmpty)
+                _head = _tail = 0;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty) throw new InvalidOperationException();
+            return _queueArray[_head];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = _head; i < _tail; i++)
+            {
+                yield return _queueArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ QueueArrayTests.cs
@@ -0,0 +1,73 @@
+﻿using Algorithm_Suite_Lib;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Suite_Lib.Tests
+{
+    [TestFixture]
+    public class QueueArrayTests
+    {
+        private QueueArray<int> _queue;
+        [SetUp]
+        public void Init()
+        {
+            _queue = new QueueArray<int>();
+        }
+
+        [Test]
+        public void Capacity_EnqueueManyItems_DoubledCapacity()
+        {
+            
+            _queue.Enqueue(1);
+            _queue.Enqueue(2);
+            _queue.Enqueue(3);
+            _queue.Enqueue(4);
+            _queue.Enqueue(5);
+
+            Assert.That(8, Is.EqualTo(_queue.Capacity));
+        }
+
+        [Test]
+        public void IsEmpty_EmptyQueue_ReturnsTrue()
+        {
+            
+            Assert.That(_queue, Is.Empty);
+        }
+
+        [Test]
+        public void Count_EnqueueOneItem_ReturnsOne()
+        {
+            
+            _queue.Enqueue(1);
+            Assert.That(_queue.IsEmpty, Is.False);
+            Assert.That(1, Is.EqualTo(_queue.Count));
+        }
+
+        [Test]
+        public void Peek_EnqueueTOneItemAndDequeue_ReturnsHeadElement()
+        {
+            
+            _queue.Enqueue(1);
+            _queue.Enqueue(2);
+            
+            Assert.That(1, Is.EqualTo(_queue.Peek()));
+        }
+
+        [Test]
+        public void Peek_EnqueueTwoItemsAndDequeue_ReturnsHeadElement()
+        {
+            
+            _queue.Enqueue(1);
+            _queue.Enqueue(2);
+
+            _queue.Dequeue();
+
+            Assert.That(2, Is.EqualTo(_queue.Peek()));
+        }
+    }
+}
