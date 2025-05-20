ccba19ad9e05e6e536dba57d3e68558b7fea1028
きよみず(KIyomizu)あゆみ(Ayumi) <kiyo.a@outlook.com>
2025-05-20 12:24:28 -0400

    Add files via upload

---
+++ BinarySearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Suite_Lib
+{
+    public class BinarySearch <T> : IEnumerable<T>
+    {
+        public static int RecBinSearch(int[] array, int value) //Recursive binary search
+        {
+            int IntRecBinSearch(int low, int high)
+            {
+                if (low >= high) return -1;
+                int mid = (low + high) / 2;
+                if (array[mid] == value) return mid;
+                if (array[mid] < value) return IntRecBinSearch(mid + 1, high);
+                return IntRecBinSearch(low, mid);
+
+            }
+            return IntRecBinSearch(0, array.Length);
+        }
+        public static int BinSearch(int[] array, int value)   //Linear binary search
+        {
+            int low = 0;
+            int high = array.Length;
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+
+                if (array[mid] == value)
+                    return mid;
+                if (array[mid] < value) low = mid + 1;
+                else high = mid;
+            }
+            return -1;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        
+    }
+}
---
+++ BinarySearchTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithm_Suite_Lib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Suite_Lib.Tests
+{
+    [TestFixture]
+    public class BinarySearchTests
+    {
+        [Test]
+        public void BinarySearch_SortedInput_ReturnsCorrectIndex()
+        {
+            int[] input = { 0, 3, 4, 7, 8, 12, 15, 22 };
+            Assert.That(2, Is.EqualTo(BinarySearch<int>.BinSearch(input, 4)));
+            Assert.That(4, Is.EqualTo(BinarySearch<int>.BinSearch(input, 8)));
+            Assert.That(6, Is.EqualTo(BinarySearch<int>.BinSearch(input, 15)));
+            Assert.That(7, Is.EqualTo(BinarySearch<int>.BinSearch(input, 22)));
+
+            Assert.That(2, Is.EqualTo(BinarySearch<int>.RecBinSearch(input, 4)));
+            Assert.That(4, Is.EqualTo(BinarySearch<int>.RecBinSearch(input, 8)));
+            Assert.That(6, Is.EqualTo(BinarySearch<int>.RecBinSearch(input, 15)));
+            Assert.That(7, Is.EqualTo(BinarySearch<int>.RecBinSearch(input, 22)));
+        }
+    }
+}
